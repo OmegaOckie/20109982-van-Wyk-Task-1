7e096b47547105517e2a46a246225fe9bfd8aa11
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-09-30 20:37:06 +0200

    Finished Q.2.2

---
+++ 20109982 van Wyk POE/20109982 van Wyk POE/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_van_Wyk_POE
+{
+    // Q.2.2 Creation of abstract Base Class Character
+    abstract class Character : Tile
+    {
+        // Variable Initialization
+        protected int HP { get; set; }
+        protected int MaxHP { get; set; }
+        protected int Damage { get; set; }
+
+        protected Tile[] characterVision = new Tile[4];
+
+        public enum Movement
+        {
+            NONE,
+            UP,
+            DOWN,
+            LEFT,
+            Right
+        }
+
+    }
+}
