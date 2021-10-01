4c7647b1594372520306881bcade5655e717a80b
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-10-01 03:32:30 +0200

    Did Q.2.6
    Left the overriden ReturnMove method for later

---
+++ 20109982 van Wyk POE/20109982 van Wyk POE/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_van_Wyk_POE
+{
+    class Hero : Character
+    {
+        /// <summary>
+        /// A constructor that receives an X and Y position and HP, but calls the
+        /// Character constructor initialiser, passing it the X and Y position.It should
+        /// then set the HP and max HP of the hero based on the parameter.
+        /// Additionally, a Hero’s damage is always 2.
+        /// </summary>
+        /// <param name="heroX"></param>
+        /// <param name="heroY"></param>
+        /// <param name="heroHP"></param>
+        public Hero(int heroX, int heroY, int heroHP) : base(heroX, heroY, 'H')
+        {
+            HP = heroHP;
+            MaxHP = heroHP;
+            Damage = 2;
+        }
+
+        public override Movement ReturnMove(Movement move)
+        {
+            if (Keyboa)
+            {
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return "$Player Stats:" +
+                "\n HP:{HP}/{MaxHP}" +
+                "\n Damage: 2" +
+                "\n [{x},{y}]";
+        }
+    }
+}
